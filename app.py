3f9d43ddddf577d1a8269713a99e83d885e2917a
Σταμάτης Σαρρηνικολάου <176748942+ssarrinik@users.noreply.github.com>
2026-02-14 14:02:41 +0200

    Fix the skills section and added the download cv feature.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
@@ -8,6 +8,11 @@ bootstrap = Bootstrap5(app)
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/download_cv")
+def download():
+    return send_file("static/stamatis_cv", as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
